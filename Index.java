420572872de83087f9696232f1d992e09c16fa1f
MielkeNicolas <mielkeroman99@gmail.com>
2022-05-04 18:42:56 +0200

    Added check string of enter symbols

--- Index.java
+++ Index.java
@@ -4,18 +4,19 @@ public class Index {
 
     public static void main (String[] args){
         String aDate;
-        int bDate;
+        int bDate = 0;
         short aSumm, bSumm, cSumm;
         Scanner in = new Scanner(System.in);
         System.out.println("Введи потрібну дату (без розділових символів): ");
         aDate = in.nextLine();
-        if (!aDate.equals("")){
+        if ((!aDate.equals("")) && (new Scanner(aDate).hasNextInt())) {
             System.out.println("Значення!");
+            bDate = Integer.parseInt(aDate);
         }
         else{
-            System.out.println("Ви нічого не ввели!");
+            System.out.println("При введенні дати зроблено помилку!");
         }
-        System.out.println(aDate);
+        System.out.println(bDate);
     }
 
 }
